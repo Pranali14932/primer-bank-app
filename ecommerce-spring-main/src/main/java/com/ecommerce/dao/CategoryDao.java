51668aa47d6c7e57dd47aeb3836554bc9c13483a
Pranali14932 <92152803+Pranali14932@users.noreply.github.com>
2021-11-18 03:36:04 -0800

    Delete ecommerce-spring-main directory

--- ecommerce-spring-main/src/main/java/com/ecommerce/dao/CategoryDao.java
+++
@@ -1,9 +0,0 @@
-package com.ecommerce.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ecommerce.modal.Category;
-
-public interface CategoryDao extends JpaRepository<Category, Long>  {
-
-}
--- ecommerce-spring-main/src/main/java/com/ecommerce/dao/OrderDao.java
+++
@@ -1,9 +0,0 @@
-package com.ecommerce.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ecommerce.modal.Order;
-
-public interface OrderDao extends JpaRepository<Order, Long> {
-
-}
